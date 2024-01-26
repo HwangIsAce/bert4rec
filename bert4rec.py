9e9dafe9180ecff76b3b69d7ada37158d09805e8
HwangIsAce <esteen196@naver.com>
2024-01-26 14:36:10 +0900

    feat attention module (-ing)

--- bert4rec.py
+++ bert4rec.py
@@ -1,35 +1,109 @@
 import torch
-from torch import nn
+from torch import nn, einsum
+from einops import rearrange
 
 from dataloader import MyDataLoader
 from constants import TRAIN_CONSTANTS
- 
+
+
+class FeedForward(nn.Module):
+    def __init__(
+            self,
+    ):
+        super().__init__()
+        self.norm = nn.LayerNorm(train_constants.EMB_DIM)
+    
+    def forward(self, x):
+        x = self.norm(x)
+
+
+class Attention(nn.Module):
+    def __init__(
+            self,
+            vocab_size,
+            emb_dim,
+            heads,
+    ):
+        super().__init__()
+        self.heads = heads
+        self.multi_head_dim = emb_dim // heads
+        self.norm = nn.LayerNorm(emb_dim)
+        self.to_qkv = nn.Linear(emb_dim, emb_dim *3)
+
+    def forward(self, x):
+        x = self.norm(x)
+
+        q, k, v = self.to_qkv(x).chunk(3, dim=-1)
+        q, k, v = map(lambda t : rearrange(t, 'b n (h d) -> b h n d', h=self.heads), (q,k,v))
+
+        sim = einsum('b h i d, b h j d -> b h i j', q,k)
+        import IPython; IPython.embed(colors="Linux"); exit(1)
+        
+
+
+    
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        vocab_size,
+        emb_dim,
+        heads,
+        num_layers= 2,
+    ):
+        super().__init__()
+
+        self.layer = nn.ModuleList([])
+
+        for _ in range(num_layers):
+            self.layer.append(nn.ModuleList([
+                Attention(vocab_size, emb_dim, heads),
+                FeedForward()
+            ]))
+
+    def forward(self, x):
+        
+        for attn, ffn in self.layer:
+            attn_out = attn(x)
+
+            x = x + attn_out
+            x = x + ffn(x)
+
+        return x
 
 class BERT4REC(nn.Module):
     def __init__(
             self,
             vocab_size,
             emb_dim,
-            max_len
+            max_len,
+            heads
     ):
         super().__init__()
 
-        self.mex_len = max_len
+        self.max_len = max_len
 
         self.item_embedder = nn.Embedding(vocab_size, emb_dim)
         self.positional_embedder = nn.Embedding(max_len+1,emb_dim)
 
-    def forward(self, x):
+        self.transformer = Transformer(vocab_size, emb_dim, heads)
 
+    def forward(self, x):
+        if x.shape[1] > self.max_len :
+            x = x[:, :self.max_len]
+        
         # item embedding
         x = self.item_embedder(x)
 
         # positional embedding
         positional_tensor = (torch.range(1, x.shape[1]) * torch.ones(x.shape[0], x.shape[1])).long()
         positional_embedding = self.positional_embedder(positional_tensor)
+        
 
         x = x + positional_embedding 
-        
+
+        # transformer
+        x = self.transformer(x)
+
         return x
 
 
@@ -43,7 +117,8 @@ if __name__ == "__main__":
 
     model = BERT4REC(train_constants.VOCAB_SIZE, 
                      train_constants.EMB_DIM, 
-                     train_constants.MAX_LEN)
+                     train_constants.MAX_LEN,
+                     heads=8)
 
     for i, batch in enumerate(train_loader):
 
